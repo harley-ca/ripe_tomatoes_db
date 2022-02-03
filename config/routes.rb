108d238aeaf00070b805fb840633fd13e7b15dc9
harley-ca <gcab022110@coderacademy.edu.au>
2022-02-03 15:17:49 +1000

    i dont even know

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/movies', to: 'movies#index'
-  get '/movies/new', to: 'movies#new'
-  get '/movies/:id', to: 'movies#show', as: 'movie'
-  post '/movies', to: 'movies#create'
-
+  # get '/movies', to: 'movies#index'
+  # get '/movies/new', to: 'movies#new'
+  # get '/movies/:id', to: 'movies#show', as: 'movie'
+  # post '/movies', to: 'movies#create'
+  resources :movies, only:[:index, :show, :new, :create] do
+    resources :reviews, only:[:new, :create]
+  end
   get '/actors', to: 'actors#index'
+
+  
 
 
 
